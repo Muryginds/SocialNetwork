80268ce046ad63d009c21689993265190d797362
Muryginds <muryginds@gmail.com>
2023-01-31 16:18:22 +0300

    user registration method modified: added mail sending function

--- src/main/java/ru/skillbox/zerone/backend/service/UserService.java
+++ src/main/java/ru/skillbox/zerone/backend/service/UserService.java
@@ -2,6 +2,11 @@ package ru.skillbox.zerone.backend.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.skillbox.zerone.backend.model.dto.response.MessageResponseDTO;
@@ -12,12 +17,16 @@ import ru.skillbox.zerone.backend.model.dto.response.CommonResponseDTO;
 import ru.skillbox.zerone.backend.model.entity.User;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JavaMailSender emailSender;
+  @Value("${spring.mail.username}")
+  private String senderMail;
 
   @Transactional
   public CommonResponseDTO<MessageResponseDTO> registerAccount(RegisterRequestDTO request) {
@@ -25,13 +34,28 @@ public class UserService {
     if (userRepository.findByEmail(request.getEmail()).isPresent()) {
       throw new UserAlreadyExistException(request.getEmail());
     }
-    CommonResponseDTO<MessageResponseDTO> response = new CommonResponseDTO<>();
-    response.setData(new MessageResponseDTO("ok"));
 
-    User user = createUserFromRequest(request);
+    var user = createUserFromRequest(request);
+    var verificationUuid = UUID.randomUUID();
+    user.setConfirmationCode(verificationUuid.toString());
+
     userRepository.save(user);
 
+    var message = createVerificationMessage(user.getEmail(), verificationUuid.toString());
+    emailSender.send(message);
+
+    CommonResponseDTO<MessageResponseDTO> response = new CommonResponseDTO<>();
+    response.setData(new MessageResponseDTO("ok"));
     return response;
+  }
+
+  public SimpleMailMessage createVerificationMessage(String mailToSend, String verifyCode) {
+    var message = new SimpleMailMessage();
+    message.setFrom(senderMail);
+    message.setTo(mailToSend);
+    message.setSubject("Verification message");
+    message.setText(String.format("Verification code that you need to input: %s", verifyCode));
+    return message;
   }
 
   public User createUserFromRequest(RegisterRequestDTO request) {
