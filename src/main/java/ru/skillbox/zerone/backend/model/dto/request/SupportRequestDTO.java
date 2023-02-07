d7d6177898b34235f3ac35027a1ea9564f8bc1e2
Vladimir Panfilov <vrpanfilov@yandex.ru>
2023-02-07 09:20:56 +0300

    Fixed regexp in the SupportRequestDTO class.

--- src/main/java/ru/skillbox/zerone/backend/model/dto/request/SupportRequestDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/request/SupportRequestDTO.java
@@ -1,22 +1,26 @@
 package ru.skillbox.zerone.backend.model.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import ru.skillbox.zerone.backend.util.ValidationUtils;
 
 @Data
 public class SupportRequestDTO {
   @JsonProperty("e_mail")
-  @Email(message = "email must be as 'aaaa@bbbb.ccc")
+  @Pattern(regexp = ValidationUtils.EMAIL_PATTERN, message = "email must match form aaa@bbb.cc")
   private String email;
   @JsonProperty("last_name")
-  @NotBlank(message = "last_name must be not empty")
+  @Size(min = 2, message = "last name should have at least 2 characters")
+  @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "last name must contain only alphabetic characters")
   private String lastName;
   @JsonProperty("message")
   @NotBlank(message = "message must be not empty")
   private String message;
   @JsonProperty("first_name")
-  @NotBlank(message = "first_name must be not empty")
+  @Size(min = 2, message = "user name must have at least 2 characters")
+  @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "user name must contain only alphabetic characters")
   private String firstName;
 }
