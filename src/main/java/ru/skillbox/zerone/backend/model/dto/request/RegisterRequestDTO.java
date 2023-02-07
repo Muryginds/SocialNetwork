bfd16537d821e5641bb57913948a485a1917130a
Muryginds <muryginds@gmail.com>
2023-02-08 00:01:57 +0300

    validation patterns fixed regarding fontend restrictions

--- src/main/java/ru/skillbox/zerone/backend/model/dto/request/RegisterRequestDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/request/RegisterRequestDTO.java
@@ -8,15 +8,15 @@ import ru.skillbox.zerone.backend.util.ValidationUtils;
 
 @Data
 public class RegisterRequestDTO {
-  @Size(min = 2, message = "user name must have at least 2 characters")
+  @Size(min = 3, message = "user name must have at least 3 characters")
   @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "user name must contain only alphabetic characters")
   private String firstName;
 
-  @Size(min = 2, message = "last name should have at least 2 characters")
+  @Size(min = 3, message = "last name should have at least 3 characters")
   @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "last name must contain only alphabetic characters")
   private String lastName;
 
-  @Size(min = 6, message = "password must have at least 6 characters")
+  @Size(min = 8, message = "password must have at least 8 characters")
   @Pattern(regexp = ValidationUtils.PASSWORD_PATTERN,
       message = "password must contain at least 1 uppercase, 1 lowercase, 1 special character and 1 digit")
   @JsonProperty("passwd1")
