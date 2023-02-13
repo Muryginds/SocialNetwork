85cb9191219b35c5ade4860a7c01f5eaad4c8155
Muryginds <muryginds@gmail.com>
2023-02-13 22:54:23 +0300

    validations modified

--- src/main/java/ru/skillbox/zerone/backend/model/dto/request/SupportRequestDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/request/SupportRequestDTO.java
@@ -10,17 +10,17 @@ import ru.skillbox.zerone.backend.util.ValidationUtils;
 @Data
 public class SupportRequestDTO {
   @JsonProperty("e_mail")
-  @Pattern(regexp = ValidationUtils.EMAIL_PATTERN, message = "email must match form aaa@bbb.cc")
+  @Pattern(regexp = ValidationUtils.EMAIL_PATTERN, message = ValidationUtils.EMAIL_PATTERN_DESCRIPTION)
   private String email;
   @JsonProperty("last_name")
-  @Size(min = 2, message = "last name should have at least 2 characters")
-  @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "last name must contain only alphabetic characters")
+  @Size(min = 3, message = "last name " + ValidationUtils.SIZE_3_PATTERN_DESCRIPTION)
+  @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "last name " + ValidationUtils.LETTERS_PATTERN_DESCRIPTION)
   private String lastName;
   @JsonProperty("message")
-  @NotBlank(message = "message must be not empty")
+  @NotBlank(message = ValidationUtils.MESSAGE_NOT_EMPTY_DESCRIPTION)
   private String message;
   @JsonProperty("first_name")
-  @Size(min = 2, message = "user name must have at least 2 characters")
-  @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "user name must contain only alphabetic characters")
+  @Size(min = 3, message = "user name " + ValidationUtils.SIZE_3_PATTERN_DESCRIPTION)
+  @Pattern(regexp = ValidationUtils.LETTERS_PATTERN, message = "user name " + ValidationUtils.LETTERS_PATTERN_DESCRIPTION)
   private String firstName;
 }
