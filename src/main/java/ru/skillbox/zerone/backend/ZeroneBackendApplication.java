c2b1c25dc2f4b27401af0522f6349db46cf0d950
Muryginds <muryginds@gmail.com>
2023-02-15 17:31:02 +0300

    @EnableAspectJAutoProxy annotation removed

--- src/main/java/ru/skillbox/zerone/backend/ZeroneBackendApplication.java
+++ src/main/java/ru/skillbox/zerone/backend/ZeroneBackendApplication.java
@@ -2,10 +2,8 @@ package ru.skillbox.zerone.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class ZeroneBackendApplication {
 
   public static void main(String[] args) {
