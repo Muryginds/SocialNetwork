ad233fcb2a2b5cfd6d48b7944d518a2faca9d710
ivan_anokhin_1 <vanesanoxin9@gmail.com>
2023-03-06 12:20:04 +0300

    Загрузка данных в БД + File + FileRepository.

--- src/main/java/ru/skillbox/zerone/backend/service/StorageService.java
+++ src/main/java/ru/skillbox/zerone/backend/service/StorageService.java
@@ -2,11 +2,10 @@ package ru.skillbox.zerone.backend.service;
 
 import com.cloudinary.Cloudinary;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skillbox.zerone.backend.model.dto.response.CommonResponseDTO;
-import ru.skillbox.zerone.backend.model.dto.response.StorageDTO;
+import ru.skillbox.zerone.backend.model.entity.File;
+import ru.skillbox.zerone.backend.repository.FileRepository;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -15,22 +14,27 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
-@Slf4j
 public class StorageService {
   private final Cloudinary cloudinary;
+  private final FileRepository fileRepository;
   private final static Map options = Map.of();
 
   public String uploadImage(MultipartFile file) {
     try {
       var uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
-      return (String) uploadResult.get("secure_url");
+      // Получаем информацию о загруженном файле
+      String publicId = (String) uploadResult.get("public_id");
+      String secureUrl = (String) uploadResult.get("secure_url");
+      String format = (String) uploadResult.get("format");
+      // Создаем объект файла и сохраняем его в БД
+      File fileUp = new File();
+      fileUp.setPublicId(publicId)
+      .setUrl(secureUrl)
+      .setFormat(format);
+      fileRepository.save(fileUp);
+      return secureUrl;
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
   }
-    public CommonResponseDTO<StorageDTO> uploadFileUrl (MultipartFile file) {
-      StorageDTO storageDTO = new StorageDTO();
-      storageDTO.setUrl(uploadImage(file));
-      return CommonResponseDTO.<StorageDTO>builder().data(storageDTO).build();
-    }
-  }
+}
