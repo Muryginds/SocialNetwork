b950f2763acdd933624ce70fc7d7008827d7a889
Muryginds <muryginds@gmail.com>
2023-03-08 19:29:41 +0300

    email validation added

--- src/main/java/ru/skillbox/zerone/backend/model/dto/request/AuthRequestDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/request/AuthRequestDTO.java
@@ -1,9 +1,12 @@
 package ru.skillbox.zerone.backend.model.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import ru.skillbox.zerone.backend.util.ValidationUtils;
 
 @Data
 public class AuthRequestDTO {
+  @Pattern(regexp = ValidationUtils.EMAIL_PATTERN, message = ValidationUtils.EMAIL_PATTERN_DESCRIPTION)
   private String email;
   private String password;
 }
