1f8c5934b4d7c790f629aeb9d875582fdee50b4c
ivan_anokhin_1 <vanesanoxin9@gmail.com>
2023-03-09 13:21:59 +0300

    Правки по комментариям.

--- src/main/java/ru/skillbox/zerone/backend/controller/StorageController.java
+++ src/main/java/ru/skillbox/zerone/backend/controller/StorageController.java
@@ -1,26 +1,24 @@
 package ru.skillbox.zerone.backend.controller;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skillbox.zerone.backend.model.dto.response.CommonResponseDTO;
 import ru.skillbox.zerone.backend.model.dto.response.StorageDTO;
 import ru.skillbox.zerone.backend.service.StorageService;
 
-
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1")
 public class StorageController {
   private final StorageService storageService;
+
   @PostMapping("/storage")
-  public CommonResponseDTO<StorageDTO> uploadImage(@RequestParam("file") MultipartFile file) {
-    StorageDTO storageDTO = new StorageDTO();
-    storageDTO.setUrl(storageService.uploadImage(file));
-    return CommonResponseDTO.<StorageDTO>builder().data(storageDTO).build();
+  public CommonResponseDTO<StorageDTO> postImage(@NotNull MultipartFile file) {
+    return storageService.uploadImage(file);
   }
 }
+
--- src/main/java/ru/skillbox/zerone/backend/service/StorageService.java
+++ src/main/java/ru/skillbox/zerone/backend/service/StorageService.java
@@ -4,6 +4,8 @@ import com.cloudinary.Cloudinary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ru.skillbox.zerone.backend.model.dto.response.CommonResponseDTO;
+import ru.skillbox.zerone.backend.model.dto.response.StorageDTO;
 import ru.skillbox.zerone.backend.model.entity.File;
 import ru.skillbox.zerone.backend.repository.FileRepository;
 
@@ -11,15 +13,13 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Map;
 
-
 @RequiredArgsConstructor
 @Service
 public class StorageService {
+  private static final Map<Object, Object> options = Map.of();
   private final Cloudinary cloudinary;
   private final FileRepository fileRepository;
-  private final static Map options = Map.of();
-
-  public String uploadImage(MultipartFile file) {
+  public CommonResponseDTO<StorageDTO> uploadImage(MultipartFile file) {
     try {
       var uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
       // Получаем информацию о загруженном файле
@@ -27,12 +27,14 @@ public class StorageService {
       String secureUrl = (String) uploadResult.get("secure_url");
       String format = (String) uploadResult.get("format");
       // Создаем объект файла и сохраняем его в БД
-      File fileUp = new File();
-      fileUp.setPublicId(publicId)
-      .setUrl(secureUrl)
-      .setFormat(format);
+      File fileUp = new File()
+          .setPublicId(publicId)
+          .setUrl(secureUrl)
+          .setFormat(format);
       fileRepository.save(fileUp);
-      return secureUrl;
+      StorageDTO storageDTO = new StorageDTO();
+      storageDTO.setUrl(secureUrl);
+      return CommonResponseDTO.<StorageDTO>builder().data(storageDTO).build();
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
