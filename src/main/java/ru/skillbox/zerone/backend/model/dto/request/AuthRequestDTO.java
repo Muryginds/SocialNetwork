66766db46b369d16700261229c4d1eaf3d5301e3
Muryginds <muryginds@gmail.com>
2023-03-09 19:21:12 +0300

    LoginService unit tests added

--- src/main/java/ru/skillbox/zerone/backend/model/dto/request/AuthRequestDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/request/AuthRequestDTO.java
@@ -1,10 +1,12 @@
 package ru.skillbox.zerone.backend.model.dto.request;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Data;
 import ru.skillbox.zerone.backend.util.ValidationUtils;
 
 @Data
+@Builder
 public class AuthRequestDTO {
   @Pattern(regexp = ValidationUtils.EMAIL_PATTERN, message = ValidationUtils.EMAIL_PATTERN_DESCRIPTION)
   private String email;
