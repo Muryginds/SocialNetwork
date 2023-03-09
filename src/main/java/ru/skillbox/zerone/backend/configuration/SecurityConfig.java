a121094a270e5bfc40749d3729ac2859cdccd0b6
Muryginds <muryginds@gmail.com>
2023-03-09 13:10:33 +0300

    cors functional style reformat

--- src/main/java/ru/skillbox/zerone/backend/configuration/SecurityConfig.java
+++ src/main/java/ru/skillbox/zerone/backend/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,8 +44,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http, FilterChainExceptionHandler filterChainExceptionHandler) throws Exception {
     http
-        .cors()
-        .and()
+        .cors(SecurityConfigurerAdapter::and)
         .csrf(AbstractHttpConfigurer::disable)
         .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
         .logout(LogoutConfigurer::permitAll)
