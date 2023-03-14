ccdb57881ba24217bcfef4bdeef72a67fe832529
Boldyrev Grigoriy <grif8585@yandex.ru>
2023-03-14 18:19:28 +0300

    косметические правки4 по замечаниям Ивана

--- src/main/java/ru/skillbox/zerone/backend/repository/TagRepository.java
+++ src/main/java/ru/skillbox/zerone/backend/repository/TagRepository.java
@@ -1,6 +1,5 @@
 package ru.skillbox.zerone.backend.repository;
 
-import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
