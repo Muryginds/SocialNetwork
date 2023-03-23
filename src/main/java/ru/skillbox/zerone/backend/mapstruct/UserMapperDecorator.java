4f383fda63b47210d5a02fc47d24fc0b4ba9605a
muryginds <muryginds@gmail.com>
2023-03-23 15:28:09 +0300

    userDTO showing real online time

--- src/main/java/ru/skillbox/zerone/backend/mapstruct/UserMapperDecorator.java
+++ src/main/java/ru/skillbox/zerone/backend/mapstruct/UserMapperDecorator.java
@@ -3,11 +3,15 @@ package ru.skillbox.zerone.backend.mapstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.skillbox.zerone.backend.model.dto.request.RegisterRequestDTO;
+import ru.skillbox.zerone.backend.model.dto.response.UserDTO;
 import ru.skillbox.zerone.backend.model.entity.Role;
 import ru.skillbox.zerone.backend.model.entity.User;
+import ru.skillbox.zerone.backend.repository.SocketIORepository;
 import ru.skillbox.zerone.backend.service.RoleService;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class UserMapperDecorator implements UserMapper {
   @Autowired
@@ -16,14 +20,25 @@ public abstract class UserMapperDecorator implements UserMapper {
   private PasswordEncoder passwordEncoder;
   @Autowired
   private RoleService roleService;
+  @Autowired
+  private SocketIORepository socketIORepository;
 
   @Override
   public User registerRequestDTOToUser(RegisterRequestDTO registerRequestDTO) {
     var user = userMapper.registerRequestDTOToUser(registerRequestDTO);
     user.setPassword(passwordEncoder.encode(user.getPassword()));
-    var roles = new ArrayList<Role>();
+    List<Role> roles = new ArrayList<>();
     roles.add(roleService.getBasicUserRole());
     user.setRoles(roles);
     return user;
+  }
+
+  @Override
+  public UserDTO userToUserDTO(User user) {
+    var userDTO = userMapper.userToUserDTO(user);
+    if (socketIORepository.findSessionByUserId(user.getId()).isPresent()) {
+      userDTO.setLastOnlineTime(LocalDateTime.now());
+    }
+    return userDTO;
   }
 }
