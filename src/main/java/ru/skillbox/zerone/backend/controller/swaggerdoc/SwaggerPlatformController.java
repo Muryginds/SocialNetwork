a9c3400b2d3fb21c43d402c380b12a01e86a19be
muryginds <muryginds@gmail.com>
2023-04-03 11:53:26 +0300

    swagger documentation added

---
+++ src/main/java/ru/skillbox/zerone/backend/controller/swaggerdoc/SwaggerPlatformController.java
@@ -0,0 +1,24 @@
+package ru.skillbox.zerone.backend.controller.swaggerdoc;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import ru.skillbox.zerone.backend.model.dto.response.BasicEntityDTO;
+import ru.skillbox.zerone.backend.model.dto.response.CommonListResponseDTO;
+
+@Tag(name = "Контроллер для загрузки списка стран, языков и городов")
+public interface SwaggerPlatformController {
+
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @Operation(summary = "Получение списка языков")
+  CommonListResponseDTO<BasicEntityDTO> getLanguages();
+
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @Operation(summary = "Получение списка городов")
+  CommonListResponseDTO<BasicEntityDTO> getCities(int countryId, String city, int offset, int itemPerPage);
+
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @Operation(summary = "Получение списка стран")
+  CommonListResponseDTO<BasicEntityDTO> getCountries(String country, int offset, int itemPerPage);
+}
--- src/main/java/ru/skillbox/zerone/backend/model/dto/response/BasicEntityDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/response/BasicEntityDTO.java
@@ -1,11 +1,15 @@
 package ru.skillbox.zerone.backend.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@Schema(description = "Модель данных с идентификатором и названием")
 public class BasicEntityDTO {
   private Long id;
-  private String title;
+  @JsonProperty("title")
+  private String name;
 }
