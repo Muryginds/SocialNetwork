6afeeff0084489549a4ea7140d5e88a1094faaaf
muryginds <muryginds@gmail.com>
2023-04-03 17:55:37 +0300

    IsFriendsDTO swagger added

--- src/main/java/ru/skillbox/zerone/backend/model/dto/request/IsFriendsDTO.java
+++ src/main/java/ru/skillbox/zerone/backend/model/dto/request/IsFriendsDTO.java
@@ -1,12 +1,15 @@
 package ru.skillbox.zerone.backend.model.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Schema(description = "Модель данных для проверки статуса 'друзья'")
 public class IsFriendsDTO {
+  @Schema(description = "Список id пользователей, относительно которых нужно проверить статус")
   @JsonProperty("user_ids")
   private List<Long> userIds;
 }
