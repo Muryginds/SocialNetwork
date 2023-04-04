a32ca900ac8a478902492e12edb3bbce944c73f8
muryginds <muryginds@gmail.com>
2023-04-04 18:04:39 +0300

    postDialogs method restored

--- src/main/java/ru/skillbox/zerone/backend/service/DialogService.java
+++ src/main/java/ru/skillbox/zerone/backend/service/DialogService.java
@@ -123,33 +123,39 @@ public class DialogService {
 
     var optionalDialog = dialogRepository.findByUserDuet(user, companion);
 
-    if (optionalDialog.isPresent()) {
-      throw new DialogException(String.format("Диалог пользователя (id = %s) с пользователем (id = %s) уже существует", user.getId(), id));
+    if (optionalDialog.isEmpty()) {
+      var dialog = Dialog.builder()
+          .sender(user)
+          .recipient(companion)
+          .build();
+      dialogRepository.save(dialog);
+
+      String firstMessageText;
+      if (user.getId().equals(companion.getId())) {
+        firstMessageText = "Это ваше персональное хранилище сообщений";
+      } else {
+        firstMessageText = String.format("%s начал(а) беседу", user.getFirstName());
+      }
+
+      var message = Message.builder()
+          .messageText(firstMessageText)
+          .dialog(dialog)
+          .author(user)
+          .build();
+      messageRepository.save(message);
+
+      socketIOService.sendMessageEvent(message);
+
+      var dialogDataDTO = dialogMapper.dialogToDialogDataDTO(dialog, message, 0, companion);
+
+      return ResponseUtils.commonResponseWithData(dialogDataDTO);
     }
 
-    var dialog = Dialog.builder()
-        .sender(user)
-        .recipient(companion)
-        .build();
-    dialogRepository.save(dialog);
-
-    String firstMessageText;
-    if (user.getId().equals(companion.getId())) {
-      firstMessageText = "Это ваше персональное хранилище сообщений";
-    } else {
-      firstMessageText = String.format("%s начал(а) беседу", user.getFirstName());
-    }
-
-    var message = Message.builder()
-        .messageText(firstMessageText)
-        .dialog(dialog)
-        .author(user)
-        .build();
-    messageRepository.save(message);
-
-    socketIOService.sendMessageEvent(message);
-
-    var dialogDataDTO = dialogMapper.dialogToDialogDataDTO(dialog, message, 0, companion);
+    var dialog = optionalDialog.get();
+    var lastMessage = messageRepository.findFirstByDialogOrderBySentTimeDesc(dialog)
+        .orElseThrow(() -> new DialogException(String.format("Для диалога с id \"%s\" не найдено сообщений", dialog.getId())));
+    int unreadCount = messageRepository.countByDialogAndAuthorAndReadStatus(dialog, companion, SENT);
+    var dialogDataDTO = dialogMapper.dialogToDialogDataDTO(dialog, lastMessage, unreadCount, companion);
 
     return ResponseUtils.commonResponseWithData(dialogDataDTO);
   }
