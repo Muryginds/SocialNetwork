70dbe3f717e2ecdf88cd4345c6e2c152129c519c
muryginds <muryginds@gmail.com>
2023-04-04 12:07:45 +0300

    swagger doc fixes

--- src/main/java/ru/skillbox/zerone/backend/controller/swaggerdoc/SwaggerFriendsController.java
+++ src/main/java/ru/skillbox/zerone/backend/controller/swaggerdoc/SwaggerFriendsController.java
@@ -1,6 +1,7 @@
 package ru.skillbox.zerone.backend.controller.swaggerdoc;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,32 +15,32 @@ public interface SwaggerFriendsController {
 
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Добавление прошло успешно"),
-      @ApiResponse(responseCode = "400", description = "Не получается добавить")
+      @ApiResponse(responseCode = "400", description = "Не получается добавить в друзья", content = @Content)
   })
   @Operation(summary = "Добавление в друзья")
   CommonResponseDTO<MessageResponseDTO> addFriend(long id);
 
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Удаление прошло успешно"),
-      @ApiResponse(responseCode = "400", description = "Не получается удалить")
+      @ApiResponse(responseCode = "400", description = "Не получается удалить из друзей", content = @Content)
   })
   @Operation(summary = "Удаление из друзей")
   CommonResponseDTO<MessageResponseDTO> removeFriend(long id);
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список друзей сформирован"))
   @Operation(summary = "Получить список друзей")
   CommonListResponseDTO<UserDTO> getFriendList(String name, @Min(0) int offset, @Min(0) int itemPerPage);
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список запросов сформирован"))
   @Operation(summary = "Получить список запросов в друзья")
   CommonListResponseDTO<UserDTO> getFriendRequestList(String name, @Min(0) int offset, @Min(0) int itemPerPage);
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список с результатами успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список ответов сформирован"))
   @Operation(summary = "Являются ли пользователи друзьями",
       description = "Получить информацию является ли пользователь другом указанных пользователей")
   CommonListResponseDTO<StatusFriendDTO> checkIsFriend(@Valid IsFriendsDTO isFriendsDTO);
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список рекоммендаций сформирован"))
   @Operation(summary = "Получить список рекомендаций", description = "Получить список персональных рекомендация для пользователя")
   CommonListResponseDTO<UserDTO> getRecommendations(@Min(0) int offset, @Min(0) int itemPerPage);
 }
--- src/main/java/ru/skillbox/zerone/backend/controller/swaggerdoc/SwaggerPlatformController.java
+++ src/main/java/ru/skillbox/zerone/backend/controller/swaggerdoc/SwaggerPlatformController.java
@@ -10,15 +10,15 @@ import ru.skillbox.zerone.backend.model.dto.response.CommonListResponseDTO;
 @Tag(name = "Контроллер для загрузки списка стран, языков и городов")
 public interface SwaggerPlatformController {
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список языков сформирован"))
   @Operation(summary = "Получение списка языков")
   CommonListResponseDTO<BasicEntityDTO> getLanguages();
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список городов сформирован"))
   @Operation(summary = "Получение списка городов")
   CommonListResponseDTO<BasicEntityDTO> getCities(int countryId, String city, int offset, int itemPerPage);
 
-  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список успешно сформирован"))
+  @ApiResponses(value = @ApiResponse(responseCode = "200", description = "Список стран сформирован"))
   @Operation(summary = "Получение списка стран")
   CommonListResponseDTO<BasicEntityDTO> getCountries(String country, int offset, int itemPerPage);
 }
