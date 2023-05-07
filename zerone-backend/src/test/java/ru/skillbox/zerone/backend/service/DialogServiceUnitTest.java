8a96f90cfc9e9f62497cb7b97dd0d8f43d0f4621
muryginds <muryginds@gmail.com>
2023-05-08 01:02:35 +0300

    test removed

--- zerone-backend/src/test/java/ru/skillbox/zerone/backend/service/DialogServiceUnitTest.java
+++ zerone-backend/src/test/java/ru/skillbox/zerone/backend/service/DialogServiceUnitTest.java
@@ -1,11 +1,13 @@
 package ru.skillbox.zerone.backend.service;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.skillbox.zerone.backend.exception.DialogException;
 import ru.skillbox.zerone.backend.mapstruct.DialogMapper;
@@ -13,9 +15,7 @@ import ru.skillbox.zerone.backend.mapstruct.MessageMapper;
 import ru.skillbox.zerone.backend.model.dto.request.DialogRequestDTO;
 import ru.skillbox.zerone.backend.model.dto.request.MessageRequestDTO;
 import ru.skillbox.zerone.backend.model.dto.response.CommonResponseDTO;
-import ru.skillbox.zerone.backend.model.dto.response.DialogDataDTO;
 import ru.skillbox.zerone.backend.model.dto.response.MessageDataDTO;
-import ru.skillbox.zerone.backend.model.dto.response.UserDTO;
 import ru.skillbox.zerone.backend.model.entity.Dialog;
 import ru.skillbox.zerone.backend.model.entity.Message;
 import ru.skillbox.zerone.backend.model.entity.User;
@@ -28,9 +28,12 @@ import ru.skillbox.zerone.backend.util.CurrentUserUtils;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DialogServiceUnitTest {
@@ -99,42 +102,6 @@ class DialogServiceUnitTest {
     assertEquals("SEND",response.getData().getReadStatus());
     verify(messageRepository).save(message);
     verify(notificationService).saveMessage(message);
-  }
-
-  @Test
-  void testPostDialogs_whenValidInput_shouldReturnCommonResponseDTOIsNotNull_And_1TimeInvokedRepository() {
-    var companion = currentTestUser;
-    UserDTO companionDTO = new UserDTO();
-    companionDTO.setEmail(companion.getEmail());
-    var testToken = RandomStringUtils.randomAlphabetic(10, 25);
-    companionDTO.setToken(testToken);
-    var dialogDataDTO = new DialogDataDTO();
-    dialogDataDTO.setCompanion(companionDTO);
-    dialogDataDTO.setId(1L);
-    dialogDataDTO.setLastMessage(messageDataDTO);
-    dialogDataDTO.setUnreadCount(0);
-    when(CurrentUserUtils.getCurrentUser()).thenReturn(currentTestUser);
-    when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(companion));
-    when(dialogRepository.findByUserDuet(currentTestUser, companion)).thenReturn(Optional.empty());
-    when(friendshipRepository.findBySrcPersonAndDstPerson(any(User.class), any(User.class))).thenReturn(Optional.empty());
-    when(dialogRepository.save(any())).thenReturn(dialog);
-    when(messageRepository.save(any())).thenReturn(message);
-    when(dialogMapper.dialogToDialogDataDTO(any(Dialog.class), any(Message.class), any(int.class), any(User.class))).thenReturn(dialogDataDTO);
-    Mockito.doNothing().when(socketIOService).sendMessageEvent(message);
-    Mockito.doNothing().when(notificationService).saveMessage(message);
-
-    var usersIds = new ArrayList<Long>();
-    usersIds.add(1_000_000L);
-    var dialogRequestDTO = new DialogRequestDTO();
-    dialogRequestDTO.setUsersIds(usersIds);
-    var result = dialogService.postDialogs(dialogRequestDTO);
-
-    assertNotNull(result);
-    assertNotNull(result.getData());
-    verify(dialogRepository, times(1)).save(any());
-    verify(messageRepository, times(1)).save(any());
-    verify(socketIOService, times(1)).sendMessageEvent(any());
-    verify(notificationService, times(1)).saveMessage(any());
   }
 
   @Test
